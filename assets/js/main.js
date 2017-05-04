0b0a55eb75b83e8e4e1d7e82eaebda85cead89e6
amalakamala <amalakamala.a@gmail.com>
2017-05-04 16:51:14 -0300

    Ejercicio 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,19 +12,82 @@ function Pokemon(nombre,color,poderDeAtaque){
 		return(this.nivelDeAmistad = this.nivelDeAmistad + valor)
 	}
 	this.atacar = function(pokemon){
-		pokemon.vida = pokemon.vida - this.poderDeAtaque
+		return pokemon.vida = pokemon.vida - this.poderDeAtaque
 	}
 }
+
 
 const Pikachu = new Pokemon ("Pikachu","amarillo",100)
 const Charmander = new Pokemon("Charmander","rojo",20)
 const Squirtle = new Pokemon("Squirtle","rojo",70)
 
-Pikachu.atacar(Charmander)
+Pikachu.atacar(Charmander);
 console.log(Charmander.vida)
 
-console.log(Charmander.mostrarPokemon())
-console.log(Pikachu.nivelDeAmistad)
-console.log(Squirtle.aumentarAmistad(20))
 
-mostrar.innerHTML = Pikachu.mostrarPokemon() + "<br>" + Charmander.mostrarPokemon() + "<br>Nivel de vida Charmander: " + Charmander.vida;
+function peleaPokemon(){
+	var selectUno = document.getElementById("elegir-pokemon").value;
+	var selectDos = document.getElementById("elegir-pokemon-dos").value;
+
+	if(selectUno == selectDos){
+		alert("No puedes hacer Pelear dos pokemones Iguales")
+		return;
+	}
+
+
+	var ataqueUno = parseInt(pedirCampo("Ingresa poder de ataque del primer Pokemon \nEl valor debe ser menor de 100"));
+	var ataqueDos = parseInt(pedirCampo("Ingresa poder de ataque del segundo Pokemon \nEl valor debe ser menor de 100"));
+	
+
+	var pimerPoke = new Pokemon (selectUno,"amarillo",ataqueUno)
+	var segundoPoke = new Pokemon (selectDos,"rojo",ataqueDos)
+
+
+	var etiquetaResultado = document.getElementById("resultado");
+	var textoResultado = selectUno + " atacó a " + selectDos + " <br> " +  selectDos + " tiene una vida de : " + pimerPoke.atacar(segundoPoke);	
+
+	etiquetaResultado.innerHTML = textoResultado;
+}
+
+
+function pedirCampo(nombreCampo){
+	var campo = "";
+	do{
+		campo = prompt("Ingrese " + nombreCampo);
+	}while(campo=="")
+	return campo;
+}
+
+
+/*
+De esta manera me falta crear atributos a traves de 'setAttribute'
+
+function crearOptionUno(elPoke){
+var optionUno = document.getElementById("elegir-pokemon");
+var optionAux = document.createElement("option");
+var namePokemon = document.createTextNode(elPoke.nombre);
+optionAux.appendChild(namePokemon);
+optionUno.appendChild(optionAux);
+}
+
+function crearOptionDos(elPoke){
+var optionUno = document.getElementById("elegir-pokemon-dos");
+var optionAux = document.createElement("option");
+var namePokemon = document.createTextNode(elPoke.nombre);
+optionAux.appendChild(namePokemon);
+optionUno.appendChild(optionAux);
+}
+
+crearOptionUno(Pikachu);
+crearOptionUno(Charmander);
+crearOptionUno(Squirtle);
+
+crearOptionDos(Pikachu);
+crearOptionDos(Charmander);
+crearOptionDos(Squirtle);
+
+
+function peleaPokemon(){
+	return ( algo + " ataco a " + algo  + " y " +  algo  + " tiene una vida de : " + vida)
+}
+*/
